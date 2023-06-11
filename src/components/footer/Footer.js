e77fb92d024622c09dcace5319f5313f0e2efcb5
tsaiteja2216 <132136631+tsaiteja2216@users.noreply.github.com>
2023-06-11 22:29:05 +0530

    okay

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        Copyright &copy; <b>2023</b> All rights reserved. Designed By Naveen Kumar
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
